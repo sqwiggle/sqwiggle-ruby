d300d0cabb9ee90c1d5bbbc2eeb2fddb39f5f6d8
Luke Roberts <email@luke-roberts.co.uk>
2013-12-01 00:12:47 +0000

    bundle gem

---
+++ lib/sqwiggle/ruby.rb
@@ -0,0 +1,7 @@
+require "sqwiggle/ruby/version"
+
+module Sqwiggle
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/sqwiggle/ruby/version.rb
@@ -0,0 +1,5 @@
+module Sqwiggle
+  module Ruby
+    VERSION = "0.0.1"
+  end
+end
