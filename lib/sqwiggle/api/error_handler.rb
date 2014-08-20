886eae6e95a4873ea583ae9907515863fc266afe
Luke Roberts <email@luke-roberts.co.uk>
2014-08-20 11:47:31 +0100

    wip

--- lib/sqwiggle/api/error_handler.rb
+++ lib/sqwiggle/api/error_handler.rb
@@ -2,15 +2,9 @@ module Sqwiggle
   module Api
     class ErrorHandler < Faraday::Response::Middleware
       def on_complete(env)
-        return nil unless env[:status] >= 400
-        attrs = JSON.parse(env[:body]) 
-        attrs = {} if attrs.empty?
-        raise Errors::BadRequestError.new(attrs)        if env[:status] == 400
-        raise Errors::AuthenticationError.new(attrs)    if env[:status] == 401
-        raise Errors::PaymentRequiredError.new(attrs)   if env[:status] == 402
-        raise Errors::AuthorizationError.new(attrs)     if env[:status] == 403
-        raise Errors::NotFoundError.new(attrs)          if env[:status] == 404
-        raise Errors::ServerError.new(attrs)            if env[:status] == 500
+        attrs = {}.merge JSON.parse(env[:body]) 
+        return if attrs[:janus] == "success" 
+        raise Error.new(attrs)
       end
     end
   end
